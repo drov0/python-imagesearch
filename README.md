bc8972255728fd0e6a3734317a0c32365c64dfcd
Martin Lees <drov0@users.noreply.github.com>
2017-08-30 15:03:48 +0200

    initial commit, code almost cleaned and commented

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+Install : 
+
+sudo apt-get install  python-xlib
+sudo apt-get install  opencv-python
---
+++ imagesearch.py
@@ -0,0 +1,157 @@
+import cv2
+import numpy as np
+import pyautogui
+import random
+import time
+
+
+'''
+
+Converts the tuple region (topx, topy, bottomx, bottomy)
+to the tuple (topx, topy, width, height)
+
+input : a tuple containing the 4 coordinates of the region to capture
+
+output : a PIL image of the area selected.
+
+'''
+def input_converter(region):
+    x1 = region[0]
+    y1 = region[1]
+    width = region[2]-x1
+    height = region[3]-y1
+
+    return pyautogui.screenshot(region=(x1,y1,width,height))
+
+
+'''
+
+Searchs for an image within an area
+
+input :
+
+image : path to the image file (see opencv imread for supported types)
+x1 : top left x value
+y1 : top left y value
+x2 : bottom right x value
+y2 : bottom right y value
+precision : the higher, the lesser tolerant and fewer false positives are found default is 0.8
+im : a PIL image, usefull if you intend to search the same unchanging region for several elements
+
+returns :
+the top left corner coordinates of the element if found as an array [x,y] or [-1,-1] if not
+
+'''
+def imagesearcharea(image, x1,y1,x2,y2, precision=0.8, im=None) :
+    if im is None :
+        im = input_converter(region=(x1, y1, x2, y2))
+        #im.save('testarea.png') usefull for debugging purposes, this will save the captured region as "testarea.png"
+
+    img_rgb = np.array(im)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(image, 0)
+
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    if (max_val < precision):
+        return [-1, -1]
+    return max_loc
+
+
+'''
+
+click on the center of an image with a bit of random.
+eg, if an image is 100*100 with an offset of 5 it may click at 52,50 the first time and then 55,53 etc
+Usefull to avoid anti-bot monitoring while staying precise.
+
+this function doesn't search for the image, it's only ment for easy clicking on the images.
+
+input :
+
+image : path to the image file (see opencv imread for supported types)
+pos : array containing the position of the top left corner of the image [x,y]
+action : button of the mouse to activate : "left" "right" "middle", see pyautogui.click documentation for more info
+time : time taken for the mouse to move from where it was to the new position
+'''
+
+def click_image(image,pos,  action, time,offset=5):
+    img = cv2.imread(image)
+    height, width, channels = img.shape
+    pyautogui.moveTo(pos[0] + r(width / 2, offset), pos[1] + r(height / 2,offset),
+                     time)
+    pyautogui.click(button=action)
+
+
+'''
+Searchs for an image on the screen
+
+input :
+
+image : path to the image file (see opencv imread for supported types)
+precision : the higher, the lesser tolerant and fewer false positives are found default is 0.8
+im : a PIL image, usefull if you intend to search the same unchanging region for several elements
+
+returns :
+the top left corner coordinates of the element if found as an array [x,y] or [-1,-1] if not
+
+'''
+def imagesearch(image, precision=0.8):
+    im = pyautogui.screenshot()
+    #im.save('testarea.png') usefull for debugging purposes, this will save the captured region as "testarea.png"
+    img_rgb = np.array(im)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(image, 0)
+    template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    if (max_val < precision):
+        return [-1,-1]
+    return max_loc
+
+
+
+
+def imagesearch_loop(image):
+    door_pos = imagesearch(image)
+    i = 0
+    while door_pos[0] == -1:
+        print(image+" not found, waiting")
+        time.sleep(r(0.5, 0.5))
+        door_pos = imagesearch(image)
+    return door_pos
+
+def waitforimage(image, timesample):
+    pos = imagesearch(image)
+
+    while(pos[0] == -1):
+        time.sleep(timesample)
+        pos = imagesearch(image)
+    return pos
+
+def waitforimage_region(image, timesample, x1,y1,x2,y2):
+    pos = imagesearcharea(image, x1,y1,x2,y2)
+
+    while(pos[0] == -1):
+        time.sleep(timesample)
+        pos = imagesearcharea(image, x1, y1, x2, y2)
+    return pos
+
+# made to compare two images
+# input : array containing the pixels (rgb)
+def imagecompare(img_rgb, template, tolerance=0.8):
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    if max_val < tolerance:
+        return False
+    else:
+        return True
+
+    return max_val
+
+def r(num, rand):
+    return num + rand*random.random()
