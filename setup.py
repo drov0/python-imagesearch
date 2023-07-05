76a50f6df1f1053191b728b3e49133094e63434e
Martin Lees <drov0@users.noreply.github.com>
2023-07-05 00:46:48 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-imagesearch",
-    version="1.2.2",
+    version="1.3.0",
     install_requires=['opencv-python', 'numpy', 'python3_xlib', 'pyautogui', 'mss'],
     author="Martin Lees",
     author_email="drov.fr@protonmail.com",
